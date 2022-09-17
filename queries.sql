49f46e73542fa76d62b165bed570cacb6114657a
satindersingh <satindersingh472@gmail.com>
2022-09-17 11:43:46 -0600

    finished

---
+++ queries.sql
@@ -0,0 +1,3 @@
+INSERT into students (name,date_of_birth)values('sam','2000-05-06'),('nav','1994-02-06'),('gem','2000-04-08'),('fer','1999-04-08');
+INSERT into class (name,`time`)values('maths','10:00:00'),('science','12:00:00');
+INSERT into student_class (student_id,class_id)values(1,1),(1,2),(2,1),(2,2),(3,1),(3,2),(4,1),(4,2);
