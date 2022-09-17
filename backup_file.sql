082c4fce9d0c95a4f2963ad756b2c186a5ffbc45
satindersingh <satindersingh472@gmail.com>
2022-09-17 11:48:16 -0600

    updated

--- backup_file.sql
+++ backup_file.sql
@@ -76,7 +76,11 @@ CREATE TABLE `student_class` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `student_id` int(10) unsigned NOT NULL,
   `class_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `student_class_FK` (`student_id`),
+  KEY `student_class_FK_1` (`class_id`),
+  CONSTRAINT `student_class_FK` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `student_class_FK_1` FOREIGN KEY (`class_id`) REFERENCES `class` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -128,4 +132,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-17 11:44:16
+-- Dump completed on 2022-09-17 11:47:59
